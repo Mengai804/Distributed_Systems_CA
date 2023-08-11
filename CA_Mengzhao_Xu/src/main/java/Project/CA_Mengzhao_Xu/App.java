39ada8ea09b94feebc6d433ac19ef099f0af9016
Mengai804 <xumengzhaoduty@gmail.com>
2023-08-11 16:34:25 +0100

    Create Maven Project

---
+++ CA_Mengzhao_Xu/src/main/java/Project/CA_Mengzhao_Xu/App.java
@@ -0,0 +1,13 @@
+package Project.CA_Mengzhao_Xu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
